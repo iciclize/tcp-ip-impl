7ab181f76994c3ffd8e78716bf34b5546e932d8f
Yuto Ino <ice.softicon30th@gmail.com>
2019-08-13 01:55:03 +0900

    section 2-3 done

---
+++ param.h
@@ -0,0 +1,25 @@
+// ETHERMTUは<net/ethernet.h>で#define ETHERMTU ETH_DATA_LENと定義されている
+// ETH_DATA_LENは<linux/if_ether.hに#define ETH_DATA_LEN 1500と定義されている
+#define    DEFAULT_MTU    (ETHERMTU)
+#define    DEFAULT_IP_TTL    (64)
+#define    DEFAULT_PING_SIZE    (64)
+
+#define    DUMMY_WAIT_MS    (100)
+#define    RETRY_COUNT    (3)
+
+typedef struct {
+  char  *device;
+  u_int8_t  mymac[6];
+  struct in_addr  myip;
+  u_int8_t  vmac[6];
+  struct in_addr  vip;
+  struct in_addr  vmask;
+  int  IpTTL;
+  int  MTU;
+  struct in_addr  gateway;
+} PARAM;
+
+int SetDefaultParam();
+int ReadParam(char *fname);
+int isTargetIPAddr(struct in_addr *addr);
+int isSameSubnet(struct in_addr *addr);
